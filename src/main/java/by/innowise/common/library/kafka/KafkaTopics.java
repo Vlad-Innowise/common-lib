075ff0396d640f11ee8ac91383e96a75d352b719
Vlad Golubev <holybay1886@gmail.com>
2025-10-10 01:26:17 +0300

    Added Kafka dead letter topics.

--- src/main/java/by/innowise/common/library/kafka/KafkaTopics.java
+++ src/main/java/by/innowise/common/library/kafka/KafkaTopics.java
@@ -3,6 +3,8 @@ package by.innowise.common.library.kafka;
 public final class KafkaTopics {
 
     public static final String ORDER_CREATED_TOPIC = "order.created";
+    public static final String ORDER_CREATED_DLT = "order.created-dlt";
     public static final String PAYMENT_CREATED_TOPIC = "payment.created";
+    public static final String PAYMENT_CREATED_DLT = "payment.created-dlt";
 
 }
